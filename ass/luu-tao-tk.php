78bc595815fe314471dc61d83ad2314d62d3411e
thienth32 <thienth32@gmail.com>
2020-08-01 08:33:11 +0700

    Update luu-tao-tk.php

--- ass/luu-tao-tk.php
+++ ass/luu-tao-tk.php
@@ -4,12 +4,30 @@ require_once "./lib/common.php";
 // xử lý dữ liệu để tạo ra tk trong csdl
 
 // 1. Nhận dữ liệu từ request
-$name = $_POST['name'];
+$name = trim($_POST['name']);
+$nameErr = "";
+// ktra rỗng
+if(strlen($name) == 0){
+    $nameErr = "Hãy nhập họ và tên";
+}
+// ktra số lượng ký tự
+if($nameErr === "" && (strlen($name) < 4 || strlen($name) > 30)){
+    $nameErr = "Độ dài họ và tên nằm trong khoảng 4 - 30 ký tự";
+}
+
 $email = $_POST['email'];
+$emailErr = "";
 $password = $_POST['password'];
+$passwordErr = "";
+
 $cfpassword = $_POST['cfpassword'];
 $birth_date = $_POST['birth_date'];
 $avatar = $_FILES['avatar'];
+
+if($nameErr.$emailErr.$passwordErr != ""){
+    header('location: ' . BASE_URL . "tao-tk.php?nameerr=$nameErr&emailerr=$emailErr&passworderr=$passwordErr");
+    die;
+}
 
 $path = "";
 // 2. Kiểm tra dữ liệu (validate)
